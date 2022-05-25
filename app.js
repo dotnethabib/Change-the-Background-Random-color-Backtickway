7ad3ca509f3b975e5dbaa08cc4953727dfdea131
Ahasan <dotnethabib@gmail.com>
2022-05-25 17:49:46 +0600

    easy way to change backgound color

---
+++ app.js
@@ -0,0 +1,11 @@
+function copyColorCode() {
+    // 'rgb(102, 71, 71)'
+    /*  document.body.style.backgroundColor  = 
+     `rgb(${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)} )` */
+
+
+     
+
+
+   
+}
